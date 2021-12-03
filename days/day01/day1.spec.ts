a033504c3bc0cb9a14a9b1390402aca4d76b3b8e
James Hunter <james.hunter@starlingbank.com>
2021-12-03 13:12:56 +0000

    Day1b

--- days/day01/day1.spec.ts
+++ days/day01/day1.spec.ts
@@ -1,5 +1,5 @@
 import theredoc from 'theredoc';
-import { process } from './day1';
+import { process, process2 } from './day1';
 import day1Input from './day1.input';
 
 describe('Day 1', () => {
@@ -23,6 +23,28 @@ describe('Day 1', () => {
     test('real puzzle', () => {
       const result = process(day1Input);
       expect(result).toBe(1316);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      expect(process2(theredoc`
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+      `)).toBe(5);
+    });
+
+    test('real puzzle', () => {
+      const result = process2(day1Input);
+      expect(result).toBe(1344);
     });
   });
 });
--- days/day01/day1.ts
+++ days/day01/day1.ts
@@ -1,14 +1,29 @@
+const parseInput = (input: string): number[] => input.split('\n').map(number => parseInt(number));
+
 export function process (input: string) {
-  const measurements = input.split('\n').map(number => parseInt(number));
+  const measurements = parseInput(input);
   let increases = 0;
-  // let decreases = 0;
   measurements.forEach((measurement, index) => {
     if (index === 0) return;
     if (measurement > measurements[index -1]) {
       increases += 1;
-    } else {
-      // decreases += 1;
     }
   });
+  return increases;
+}
+
+const generateWindowCalculatorFor = (measurements: number[]) => (index: number) => {
+  return measurements[index] + measurements[index - 1] + measurements[index - 2];
+}
+
+export function process2 (input: string): number {
+  const measurements = parseInput(input);
+  const calculateWindowSumAt = generateWindowCalculatorFor(measurements);
+  let increases = 0;
+  for (let index = 3; index < measurements.length; index += 1) {
+    if (calculateWindowSumAt(index) > calculateWindowSumAt(index - 1)) {
+      increases += 1;
+    }
+  }
   return increases;
 }
