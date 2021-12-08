2a0a5b53b454200a088f03a6ce02eff6a04c8982
James Hunter <james.hunter@starlingbank.com>
2021-12-08 21:51:24 +0000

    Day7b

--- days/day07/day7.spec.ts
+++ days/day07/day7.spec.ts
@@ -1,5 +1,4 @@
-// import theredoc from 'theredoc';
-import { solvePart1 } from './day7';
+import { solvePart1, solvePart2, fuelForDistance } from './day7';
 import day7Input from './day7.input';
 
 describe('Day 7', () => {
@@ -12,6 +11,27 @@ describe('Day 7', () => {
     test('real puzzle', () => {
       const result = solvePart1(day7Input);
       expect(result).toBe(348664);
+    });
+  });
+
+  describe('part 1', () => {
+    test('test case', () => {
+      const input = '16,1,2,0,4,2,7,1,2,14'
+      expect(solvePart2(input)).toBe(168);
+    });
+
+    test('real puzzle', () => {
+      expect(solvePart2(day7Input)).toBe(348664);
+    });
+  });
+
+  describe('fuelForDistance', () => {
+    test('gets the right fuel count for a given distance', () => {
+      expect(fuelForDistance(1)).toBe(1);
+      expect(fuelForDistance(2)).toBe(3);
+      expect(fuelForDistance(3)).toBe(6);
+      expect(fuelForDistance(4)).toBe(10);
+      expect(fuelForDistance(5)).toBe(15);
     });
   });
 });
--- days/day07/day7.ts
+++ days/day07/day7.ts
@@ -16,3 +16,23 @@ export function solvePart1 (input: string): number {
   const bestPosition = Object.keys(numbers).sort((a, b) => counts[a] - counts[b])[0];
   return counts[bestPosition];
 }
+
+export const fuelForDistance = (distance: number): number => {
+  return [...new Array(distance)].reduce((total, __, index) => {
+    return total + 1 + index;
+  }, 0);
+}
+
+export function solvePart2 (input: string): number {
+  const numbers = input.split(',').map(number => parseInt(number)).sort(inNumericOrder);
+  const counts = createCounter();
+
+  for (let position = (numbers.at(0) || 0); position <= (numbers.at(-1) || 0); position += 1) {
+    counts[position] = numbers.reduce((total, number) => {
+      return total + fuelForDistance(Math.abs(number - position));
+    }, 0);
+  }
+
+  const bestPosition = Object.keys(numbers).sort((a, b) => counts[a] - counts[b])[0];
+  return counts[bestPosition];
+}
