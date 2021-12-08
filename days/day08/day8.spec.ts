5d0f22750aabbcce0114c117bbb7d18d90566cc8
James Hunter <james.hunter@starlingbank.com>
2021-12-08 22:49:51 +0000

    Day8b

--- days/day08/day8.spec.ts
+++ days/day08/day8.spec.ts
@@ -1,5 +1,5 @@
 import theredoc from 'theredoc';
-import { solvePart1 } from './day8';
+import { solvePart1, solvePart2 } from './day8';
 import day8Input from './day8.input';
 
 describe('Day 8', () => {
@@ -22,6 +22,27 @@ describe('Day 8', () => {
     test('real puzzle', () => {
       const result = solvePart1(day8Input);
       expect(result).toBe(488);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      const input = theredoc`
+        be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`;
+      expect(solvePart2(input)).toBe(61229);
+    });
+
+    test('real puzzle', () => {
+      expect(solvePart2(day8Input)).toBe(1040429);
     });
   });
 });
--- days/day08/day8.ts
+++ days/day08/day8.ts
@@ -16,3 +16,88 @@ export function solvePart1 (input: string): number {
     digit.length === 7
   )).length;
 }
+
+
+//  aaaa  
+// b    c
+// b    c
+//  dddd  
+// e    f
+// e    f
+//  gggg  
+
+interface Line {
+  uniqueSignalPatterns: string[];
+  outputDigits: string[]
+}
+const parseDigits = (input: string): string[] => input.split(' ').map(digit => digit.split('').sort().join(''));
+const toLine = (inputLine: string): Line => {
+  const [usp, output] = inputLine.split(' | ');
+  return {
+    uniqueSignalPatterns: parseDigits(usp),
+    outputDigits: parseDigits(output)
+  };
+}
+
+type Lookup = Record<string,number>;
+const lookupTableForLine = (line: Line): Lookup => {
+  const remainingValues = new Set(line.uniqueSignalPatterns);
+
+  const one = line.uniqueSignalPatterns.find(digit => digit.length === 2) || '';
+  remainingValues.delete(one);
+  const four = line.uniqueSignalPatterns.find(digit => digit.length === 4) || '';
+  remainingValues.delete(four);
+  const seven = line.uniqueSignalPatterns.find(digit => digit.length === 3) || '';
+  remainingValues.delete(seven)
+  const eight = line.uniqueSignalPatterns.find(digit => digit.length === 7) || '';
+  remainingValues.delete(eight);
+
+  const oneParts = one.split('');
+  const f = oneParts.find(part => line.uniqueSignalPatterns.filter(digit => !digit.includes(part)).length === 1) || '';
+  const c = oneParts.find(part => part !== f) || '';
+  
+  const two = line.uniqueSignalPatterns.find(digit => !digit.includes(f)) || '';
+  remainingValues.delete(two);
+  const five = line.uniqueSignalPatterns.find(digit => !digit.includes(c) && digit.length === 5) || '';
+  remainingValues.delete(five);
+  const six = line.uniqueSignalPatterns.find(digit => !digit.includes(c) && digit.length === 6) || '';
+  remainingValues.delete(six);
+
+  const three = line.uniqueSignalPatterns.find(digit => digit.length === 5 && remainingValues.has(digit)) || '';
+  remainingValues.delete(three);
+
+  const bd = four.split('').filter(letter => !one.includes(letter));
+  const nine = line.uniqueSignalPatterns.find(digit => remainingValues.has(digit) && digit.includes(bd[0]) && digit.includes(bd[1])) || '';
+  remainingValues.delete(nine);
+
+  if (remainingValues.size !== 1) console.error('!!!!!!!');
+
+  const zero = Array.from(remainingValues)[0];
+
+  return {
+    [zero]: 0,
+    [one]: 1,
+    [two]: 2,
+    [three]: 3,
+    [four]: 4,
+    [five]: 5,
+    [six]: 6,
+    [seven]: 7,
+    [eight]: 8,
+    [nine]: 9
+  };
+};
+
+const sum = (a: number, b: number): number => a + b;
+
+const outputValueForLine = (line: Line): number => {
+  const lookup = lookupTableForLine(line);
+  const realDigits = line.outputDigits.map(digit => lookup[digit]);
+  return parseInt(realDigits.map(digit => String(digit)).join(''));
+};
+
+export function solvePart2 (input: string): number {
+  const lines = input.split('\n').map(toLine);
+  const outputValues = lines.map(outputValueForLine);
+  return outputValues.reduce(sum);
+}
