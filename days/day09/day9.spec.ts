1711b4a8722e154a077f41b2633e13f7511d9a91
James Hunter <james.hunter@starlingbank.com>
2021-12-11 20:15:51 +0000

    Failed day9b

--- days/day09/day9.spec.ts
+++ days/day09/day9.spec.ts
@@ -1,5 +1,5 @@
 import theredoc from 'theredoc';
-import { solvePart1 } from './day9';
+import { solvePart1, solvePart2 } from './day9';
 import day9Input from './day9.input';
 
 describe('Day 9', () => {
@@ -18,6 +18,24 @@ describe('Day 9', () => {
     test('real puzzle', () => {
       const result = solvePart1(day9Input);
       expect(result).toBe(524);
+    });
+  });
+
+  describe('part 2', () => {
+    test('test case', () => {
+      const input = theredoc`
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
+      `;
+      expect(solvePart2(input)).toBe(1134);
+    });
+
+    test('real puzzle', () => {
+      const result = solvePart2(day9Input);
+      expect(result).toBe(734095);
     });
   });
 });
--- days/day09/day9.ts
+++ days/day09/day9.ts
@@ -1,8 +1,25 @@
 type Grid = number[][];
+type Point = [y: number, x: number];
 
 const parseInput = (input: string): Grid => {
   return input.split('\n').map(line => line.split('').map(number => parseInt(number)));
 };
+
+const grabNeighbours = (grid: Grid, point: Point): number[] => {
+  const [y, x] = point;
+  return [
+    grid[y - 1]?.[x],
+    grid[y + 1]?.[x],
+    grid[y]?.[x - 1],
+    grid[y]?.[x + 1]
+  ];
+};
+
+const checkForLowestPoint = (neighbours: number[], position: number): boolean => {
+  return neighbours
+    .filter(neighbour => neighbour !== undefined)
+    .every(neighbour => neighbour > position);
+}
 
 export function solvePart1 (input: string): number {
   const grid = parseInput(input);
@@ -12,20 +29,55 @@ export function solvePart1 (input: string): number {
     for (let x = 0; x < grid[y].length; x += 1) {
       const position = grid[y][x];
 
-      const neighbours = [
-        grid[y - 1]?.[x],
-        grid[y + 1]?.[x],
-        grid[y]?.[x - 1],
-        grid[y]?.[x + 1]
-      ];
+      const neighbours = grabNeighbours(grid, [y, x]);
 
-      const isLowestPoint = neighbours
-        .filter(neighbour => neighbour !== undefined)
-        .every(neighbour => neighbour > position);
+      const isLowestPoint = checkForLowestPoint(neighbours, position);
 
       if (isLowestPoint) riskLevels.push(position + 1);
     }
   }
 
   return riskLevels.reduce((sum, number) => sum + number, 0);
+}
+
+const isUphillOf = (grid: Grid, position: number, y: number, x: number): boolean => {
+  return grid[y]?.[x] !== 9 && grid[y]?.[x] === position + 1;
+};
+
+const findPointsUphill = (grid: Grid, point: Point): Point[] => {
+  const [y, x] = point;
+  const position = grid[y][x];
+
+  const uphill: Point[] = []
+  if (isUphillOf(grid, position, y - 1, x)) uphill.push([y - 1, x]);
+  if (isUphillOf(grid, position, y + 1, x)) uphill.push([y + 1, x]);
+  if (isUphillOf(grid, position, y, x - 1)) uphill.push([y, x - 1]);
+  if (isUphillOf(grid, position, y, x + 1)) uphill.push([y, x + 1]);
+
+  return [
+    ...uphill,
+    ...uphill.map(uphillPoint => findPointsUphill(grid, uphillPoint)).flat()
+  ];
+};
+
+export function solvePart2 (input: string): number {
+  const grid = parseInput(input);
+  const basinSizes: number[] = [];
+
+  for (let y = 0; y < grid.length; y += 1) {
+    for (let x = 0; x < grid[y].length; x += 1) {
+      const position = grid[y][x];
+      const neighbours = grabNeighbours(grid, [y, x]);
+      const isLowestPoint = checkForLowestPoint(neighbours, position);
+      
+      if (isLowestPoint) {
+        const allBasinPoints = findPointsUphill(grid, [y, x]);
+        const basinPoints = new Set(allBasinPoints.map(point => `${point[0]},${point[1]}`))
+        basinSizes.push(basinPoints.size + 1);
+      }
+    }
+  }
+
+  const sortedBasins = basinSizes.sort((a, b) => b - a);
+  return sortedBasins[0] * sortedBasins[1] * sortedBasins[2];
 }
