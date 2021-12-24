3518c0563e73dbec4074cda5ebaeea7888ca5670
James Hunter <james.hunter@starlingbank.com>
2021-12-24 17:47:29 +0000

    Day13a

---
+++ days/day13/day13.spec.ts
@@ -0,0 +1,38 @@
+import theredoc from 'theredoc';
+import { solvePart1 } from './day13';
+import day13Input from './day13.input';
+
+describe('Day 13', () => {
+  describe('part 1', () => {
+    test('test case', () => {
+      const input = theredoc`
+        6,10
+        0,14
+        9,10
+        0,3
+        10,4
+        4,11
+        6,0
+        6,12
+        4,1
+        0,13
+        10,12
+        3,4
+        3,0
+        8,4
+        1,10
+        2,14
+        8,10
+        9,0
+        
+        fold along y=7
+        fold along x=5`;
+      expect(solvePart1(input)).toBe(17);
+    });
+
+    test('real puzzle', () => {
+      const result = solvePart1(day13Input);
+      expect(result).toBe(687);
+    });
+  });
+});
---
+++ days/day13/day13.ts
@@ -0,0 +1,81 @@
+class Grid {
+  #grid: boolean[][];
+
+  constructor () {
+    this.#grid = [[]];
+  }
+
+  mark (x: number, y: number) {
+    if (y >= this.#grid.length) {
+      for (let index = this.#grid.length; index <= y; index += 1) {
+        this.#grid.push([])
+      }
+    }
+    if (x >= this.#grid[y].length) {
+      for (let index = this.#grid[y].length; index <= x; index += 1) {
+        this.#grid[y].push(false)
+      }
+    }
+    this.#grid[y][x] = true;
+  }
+
+  fold (fold: Fold) {
+    if (fold.axis === 'y') {
+      for (let index = 1; index < this.#grid.length - fold.number; index += 1) {
+        this.#grid[fold.number + index].forEach((cell, cellIndex) => {
+          this.#grid[fold.number - index][cellIndex] ||= cell;
+        });
+      }
+      this.#grid.splice(fold.number);
+    } else {
+      for (const row of this.#grid) {
+        for (let index = 1; index < row.length - fold.number; index += 1) {
+          row[fold.number - index] ||= row[fold.number + index];
+        }
+        row.splice(fold.number);
+      }
+    }
+  }
+
+  get pointCount (): number {
+    return this.#grid.flat().filter(Boolean).length;
+  }
+
+  toString (): string {
+    return this.#grid.map(line => line.map(cell => cell ? '#' : '.').join('')).join('\n');
+  }
+}
+
+interface Fold {
+  axis: 'x' | 'y';
+  number: number;
+}
+
+interface ParsedInput {
+  grid: Grid;
+  folds: Fold[];
+}
+
+const parseInput = (input: string): ParsedInput => {
+  const [pointLines, foldLines] = input.split('\n\n');
+  const grid = new Grid();
+  pointLines.split('\n').forEach(line => {
+    const [x, y] = line.split(',');
+    grid.mark(parseInt(x), parseInt(y));
+  });
+  const folds = foldLines.split('\n').map((line): Fold => {
+    const [, axis, number] = line.match(/fold along ([xy])=(\d+)/) as RegExpMatchArray;
+    return { axis: axis as 'x' | 'y', number: parseInt(number) };
+  });
+  return { grid, folds };
+};
+
+export function solvePart1 (input: string): number {
+  const { grid, folds } = parseInput(input);
+  grid.fold(folds[0]);
+  // folds.forEach(fold => {
+  //   grid.fold(fold);
+  // });
+  
+  return grid.pointCount;
+}
